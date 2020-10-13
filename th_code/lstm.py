c6d127d994dd5436ff3dae651bb64987f758437e
tohe91 <joeyontour3@gmx.de>
2020-10-13 11:35:47 +0200

    model creation on cpu instead of gpu

--- th_code/lstm.py
+++ th_code/lstm.py
@@ -38,8 +38,8 @@ def split_path(path):
 
 def train_models(path, file):
 
-    physical_devices = tf.config.list_physical_devices('GPU') 
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#    physical_devices = tf.config.list_physical_devices('GPU') 
+#    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     csv_files = get_resampled_data(path, file)
     
